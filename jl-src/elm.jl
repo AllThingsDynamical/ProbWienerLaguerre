def46fbc71911ff63c2a6d07e15320ac08b96113
Rahul Manavalan <rahul.manavalan@matstat.lu.se>
2026-02-10 14:32:52 +0100

    implement bayesian estimators.

--- jl-src/elm.jl
+++ jl-src/elm.jl
@@ -68,6 +68,43 @@ function elm_estimator(X::Matrix{T}, Y::Matrix{T},
     return construct_elm_estimator(a, W, b)
 end
 
+function bayesian_elm_estimator(X::Matrix{T}, Y::Matrix{T},
+                                num_features::Int = 1200;
+                                α::T = one(T),
+                                σ2::T = T(1e-3)) where {T}
+
+    d = size(X,1)
+    params = (num_features, d, zero(T))
+    Φ, W, b = rff_feature_matrix(X, params)
+
+    M, K = size(Φ)
+    n, My = size(Y)
+    @assert My == M
+
+    A = α * I(K) + (one(T)/σ2) * (Φ' * Φ)
+    F = cholesky(Symmetric(Matrix(A)))
+
+    m = (one(T)/σ2) * (F \ (Φ' * Y'))
+
+    ψ = x -> cos.(W' * x .+ b)
+
+    estimator = function(x::Matrix{T})
+        Φx = ψ(x)
+        μ = m' * Φx
+
+        mtest = size(x, 2)
+        v = zeros(T, mtest)
+        for j = 1:mtest
+            z = F.L \ Φx[:, j]
+            v[j] = dot(z, z)
+        end
+        return μ, v
+    end
+
+    return estimator
+end
+
+
 begin
     d::Int = 10
     n::Int = 3
@@ -80,4 +117,14 @@ begin
     Y
     figure = plot(Y[:], Y[:], ms=0.1)
     scatter!(Y[:], y_pred[:], ms=0.1)
+end
+
+begin
+    estimator = bayesian_elm_estimator(X, Y, 1200)
+    mu_y_pred, var_y_pred = estimator(X)
+    mu_y_pred
+    Y
+    figure = plot(mu_y_pred[1, :], ribbon=var_y_pred)
+    display(figure)
+    var_y_pred
 end
--- jl-src/rff.jl
+++ jl-src/rff.jl
@@ -60,6 +60,41 @@ function rff_estimator(X::Matrix{T}, Y::Matrix{T},
     return construct_rff_estimator(a, W, b)
 end
 
+function bayesian_rff_estimator(X::Matrix{T}, Y::Matrix{T},
+                                num_features::Int = 1200;
+                                ρ::T = one(T),
+                                α::T = one(T),
+                                σ2::T = T(1e-3)) where {T}
+
+    params = (num_features, ρ, zero(T))
+    Φ, W, b = rff_feature_matrix(X, params)
+
+    M, K = size(Φ)
+    n, My = size(Y)
+    @assert My == M
+
+    A = α * I(K) + (one(T)/σ2) * (Φ' * Φ)
+    F = cholesky(Symmetric(Matrix(A)))
+
+    m = (one(T)/σ2) * (F \ (Φ' * Y'))
+
+    ψ = x -> cos.(W' * x .+ b)
+
+    estimator = function(x::Matrix{T})
+        Φx = ψ(x)
+        μ = m' * Φx
+
+        mtest = size(x, 2)
+        v = zeros(T, mtest)
+        for j = 1:mtest
+            z = F.L \ Φx[:, j]
+            v[j] = dot(z, z)
+        end
+        return μ, v
+    end
+
+    return estimator
+end
 
 begin
     d::Int = 10
@@ -67,10 +102,22 @@ begin
     M::Int = 10_00
     X = randn(d, M)
     Y = randn(n, M)
+end
 
+begin
     estimator = rff_estimator(X, Y, 1200)
     y_pred = estimator(X)
     Y
     figure = plot(Y[:], Y[:], ms=0.1)
     scatter!(Y[:], y_pred[:], ms=0.1)
+end
+
+begin
+    estimator = bayesian_rff_estimator(X, Y, 1200)
+    mu_y_pred, var_y_pred = estimator(X)
+    mu_y_pred
+    Y
+    figure = plot(mu_y_pred[1, :], ribbon=var_y_pred)
+    display(figure)
+    var_y_pred
 end
